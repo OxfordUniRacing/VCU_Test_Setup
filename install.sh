8ed46ceedb82524972fe37f3186f2c77fdbe8227
Alan Chang <tcode2k16@gmail.com>
2021-11-10 15:04:33 +0000

    add python-can to install script

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ apt update -y
 apt upgrade -y
 apt install -y telnetd
 
+# python dependencies
+pip3 install python-can
+
 cp setup-boot.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable setup-boot.service
